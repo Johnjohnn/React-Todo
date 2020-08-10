33cb71c36871a50baaa59ea81d7f96e38294e0b9
Johnjohnn <conde2010@aol.com>
2020-08-10 02:05:48 -0400

    almost done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,63 @@
 import React from 'react';
 
+
+const doList = [
+  {
+    task: 'Organize Garage',
+    id: 1528817077286,
+    completed: false
+  },
+  {
+    task: 'Bake Cookies',
+    id: 1528817084358,
+    completed: false
+  }
+];
+
+
+
+
+
 class App extends React.Component {
   // you will need a place to store your state in this component.
+  constructor(){       // constructor with state 
+   super();
+   this.state ={
+   doList 
+
+     }
+ 
+
+
+  }
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+toggleDoList = duty => {
+console.log (duty)
+
+this.setState({
+doList: this.state.doList.map(todo =>{
+if(duty === duty.id) {
+ Return {
+ ...task,
+ 
+
+ }
+
+
+
+      }
+
+
+    })
+  
+  
+  })
+
+
+}
+
+
   render() {
     return (
       <div>
